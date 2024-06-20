2d1d677b7833e0c6205c263db561cc74aa0a13b2
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-06-19 21:51:43 -0400

    Update StereoSeq.py

--- Benchmark_Validation_Tests/StereoSeq.py
+++ Benchmark_Validation_Tests/StereoSeq.py
@@ -1,4 +1,3 @@
-import torch
 import pandas as pd
 import numpy as np
 import scanpy as sc
@@ -8,94 +7,29 @@ import warnings
 warnings.filterwarnings("ignore")
 from sklearn.metrics.cluster import normalized_mutual_info_score
 import multiprocessing as mp
-from mclustpy import mclustpy
-from GraphST import GraphST
-from tPCA import tPCA_embedding
-from sklearn.decomposition import PCA
+from TAST_GraphST import TAST_GraphST
 
 #For StereoSeq, we combine topological features with the GraphST embedding
-# Run device，by default, the package is implemented on 'cpu'. We recommend using GPU.
-device = torch.device('cuda:3' if torch.cuda.is_available() else 'cpu')
-
 section_id = sys.argv[1]
 
-if section_id=='Stereo-Seq_E95_E1S1':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/Stereo-Seq/E9.5/E9.5_E1S1.MOSTA_20240319045807.h5ad')
-if section_id=='Stereo-Seq_E95_E2S1':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/Stereo-Seq/E9.5/E9.5_E2S1.MOSTA_20240319045818.h5ad')
-if section_id=='Stereo-Seq_E95_E2S2':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/Stereo-Seq/E9.5/E9.5_E2S2.MOSTA_20240319045821.h5ad')
-if section_id=='Stereo-Seq_E95_E2S3':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/Stereo-Seq/E9.5/E9.5_E2S3.MOSTA_20240319045823.h5ad')
-if section_id=='Stereo-Seq_E95_E2S4':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/Stereo-Seq/E9.5/E9.5_E2S4.MOSTA_20240319045824.h5ad')
-if section_id=='Stereo-Seq_E10_E1S1':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/Stereo-Seq/E10.5/E10.5_E1S1.MOSTA_20240319045825.h5ad')
-if section_id=='Stereo-Seq_E10_E1S2':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/Stereo-Seq/E10.5/E10.5_E1S2.MOSTA_20240319102119.h5ad')
-if section_id=='Stereo-Seq_E10_E1S3':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/Stereo-Seq/E10.5/E10.5_E1S3.MOSTA_20240319102125.h5ad')
-
-adata.var_names_make_unique()
-
-#Extract tPCA parameters
-rootPath = '/mnt/home/cottre61/TAST/' 
-df = pd.read_csv(rootPath+'params.csv')
-section_id = sys.argv[1]
-dataset_row = df[df['Dataset'] == section_id].iloc[0]
-
-zeta = [float(dataset_row['zeta1']), float(dataset_row['zeta2']), float(dataset_row['zeta3']),
-        float(dataset_row['zeta4']), float(dataset_row['zeta5']), float(dataset_row['zeta6']),
-        float(dataset_row['zeta7']), float(dataset_row['zeta8'])]
-zeta = np.asarray(zeta)
-print('zeta:',zeta)
-k = int(dataset_row['k'])
-print('k:', k)
-beta = float(dataset_row['beta'])
-print('beta:', beta)
-gamma = float(dataset_row['gamma'])
-print('gamma:', gamma)
-n_clusters=int(dataset_row['clusters'])
+adata = sc.read('./Data/Stereo-Seq/'+section_id+'.h5ad')
 
 #Validation
 nmi_list=[1,2,3,4,5,6,7,8,9,10]
 
 for i in range(10):
     adata2 = adata.copy()
-    model = GraphST.GraphST(adata2, datatype='Stereo', device=device)
-    # run model
-    adata2 = model.train()
+    n = adata2.obs['ground_truth'].nunique()
+    adata2 = TAST_GraphST(adata = adata2, consensus = True, n_clusters = n, ground_truth = adata2.obs['ground_truth'])
 
-    #Normalization
-    sc.pp.highly_variable_genes(adata2, flavor="seurat_v3", n_top_genes=3000)
-
-    #Perform tPCA
-    adata_highly_variable = adata2[:, adata2.var['highly_variable']]
-    X = adata_highly_variable.X
-    X = X.toarray()
-
-    #Embedding
-    Q = tPCA_embedding(X, beta, gamma, k, zeta)
-
-    #Feature Concatenation
-    pca = PCA(n_components=20, random_state=42) 
-    embedding = pca.fit_transform(adata2.obsm['emb'].copy())
-    print(embedding.shape)
-    Q3 = np.concatenate((Q,embedding), axis = 1)
-
-    #McClust Clustering
-    res = mclustpy(np.real(Q3), G=n_clusters, modelNames='EEE', random_seed=2020)
-    mclust_res = res['classification']
-    adata2.obs['mclust'] = mclust_res
-
-    NMI = normalized_mutual_info_score(mclust_res, adata.obs['ground_truth'].values)
+    NMI = normalized_mutual_info_score(adata2.obs['consensus_mclust'].values,  adata2.obs['ground_truth'].values)
     print('TAST NMI = %.5f' %NMI)
     nmi_list[i] = NMI
 
 mean = np.mean(nmi_list)
 print('Average TAST NMI:', mean)
 
-#Write results to CSV
+#Write results to CSV for easy check
 results = {
         'Dataset': [section_id],
         'TAST NMI': [mean]
