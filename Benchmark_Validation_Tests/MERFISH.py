d965247f7792b55283ff3d091f0fa91e4d062e14
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-06-19 21:44:19 -0400

    Update MERFISH.py

--- Benchmark_Validation_Tests/MERFISH.py
+++ Benchmark_Validation_Tests/MERFISH.py
@@ -1,4 +1,3 @@
-import STAGATE_pyG as STAGATE
 import pandas as pd
 import numpy as np
 import scanpy as sc
@@ -7,83 +6,30 @@ import os
 import sys
 import warnings
 warnings.filterwarnings("ignore")
-from tPCA import tPCA_embedding
-from mclustpy import mclustpy
+from TAST_GATE import TAST_GATE
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
 #For MERFISH, we combine topological features with the STAGATE embedding
 #Load Data
 section_id = sys.argv[1]
-if section_id =='MERFISH_Data24':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/MERFISH/Data24/MERFISH_0.24_20240319045320.h5ad')
-if section_id =='MERFISH_Data19':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/MERFISH/Data19/MERFISH_0.19_20240319045434.h5ad')
-if section_id =='MERFISH_Data14':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/MERFISH/Data14/MERFISH_0.14_20240319045431.h5ad')
-if section_id =='MERFISH_Data9':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/MERFISH/Data9/MERFISH_0.09_20240319045428.h5ad')
-if section_id =='MERFISH_Data4':
-    adata = sc.read('/mnt/home/cottre61/TAST/Data/MERFISH/Data4/MERFISH_0.04_20240319045554.h5ad')
-
-#Normalization
-sc.pp.normalize_total(adata, target_sum=1e4)
-sc.pp.log1p(adata)
-
-#Extract tPCA parameters
-rootPath = '/mnt/home/cottre61/TAST/' 
-df = pd.read_csv(rootPath+'params.csv')
-section_id = sys.argv[1]
-dataset_row = df[df['Dataset'] == section_id].iloc[0]
-
-zeta = [float(dataset_row['zeta1']), float(dataset_row['zeta2']), float(dataset_row['zeta3']),
-        float(dataset_row['zeta4']), float(dataset_row['zeta5']), float(dataset_row['zeta6']),
-        float(dataset_row['zeta7']), float(dataset_row['zeta8'])]
-zeta = np.asarray(zeta)
-print('zeta:',zeta)
-k = int(dataset_row['k'])
-print('k:', k)
-beta = float(dataset_row['beta'])
-print('beta:', beta)
-gamma = float(dataset_row['gamma'])
-print('gamma:', gamma)
-n_clusters=int(dataset_row['clusters'])
+adata = sc.read('./Data/MERFISH/'+section_id+'.h5ad')
 
 #Validation
 nmi_list=[1,2,3,4,5,6,7,8,9,10]
 
 for i in range(10):
     adata2 = adata.copy()
-    STAGATE.Cal_Spatial_Net(adata2, rad_cutoff=75)
-    STAGATE.Stats_Spatial_Net(adata2)
-    adata = STAGATE.train_STAGATE(adata2)
+    n = adata2.obs['ground_truth'].nunique()
+    adata2 = TAST_GATE(adata = adata2, consensus = True, n_clusters = n, spatial_rad_cutoff=75, ground_truth = adata2.obs['ground_truth'])
 
-    #Perform tPCA
-    X = adata2.X
-    X = X.toarray()
-
-    #Embedding
-    Q = tPCA_embedding(X, beta, gamma, k, zeta)
-
-    #Feature Concatenation
-    Q2 = adata2.obsm['STAGATE']
-    print(Q2.shape)
-    Q3 = np.concatenate((Q,Q2), axis = 1)
-    print(Q3.shape)
-    adata2.obsm['TAST'] = Q3
-
-    #McClust Clustering
-    res = mclustpy(np.real(Q3), G=n_clusters, modelNames='EEE', random_seed=2020)
-    mclust_res = res['classification']
-    adata2.obs['mclust'] = mclust_res
-
-    NMI = normalized_mutual_info_score(mclust_res,  adata.obs['ground_truth'].values)
+    NMI = normalized_mutual_info_score(adata2.obs['consensus_mclust'].values,  adata2.obs['ground_truth'].values)
     print('TAST NMI = %.5f' %NMI)
     nmi_list[i] = NMI
 
 mean = np.mean(nmi_list)
 print('Average TAST NMI:', mean)
 
-#Write results to CSV
+#Write results to CSV for easy check
 results = {
         'Dataset': [section_id],
         'TAST NMI': [mean]
