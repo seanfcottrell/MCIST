e2184ac4b6566ed8fd430adb36209e7792bd583e
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-10-18 13:15:16 -0400

    Update StereoSeq.py

--- Benchmark_Validation_Tests/StereoSeq.py
+++ Benchmark_Validation_Tests/StereoSeq.py
@@ -7,7 +7,7 @@ import warnings
 warnings.filterwarnings("ignore")
 from sklearn.metrics.cluster import normalized_mutual_info_score
 import multiprocessing as mp
-from TAST_GraphST import TAST_GraphST
+from MCSER_GraphST import MCSER_GraphST
 
 #For StereoSeq, we combine topological features with the GraphST embedding
 section_id = sys.argv[1]
@@ -20,19 +20,19 @@ nmi_list=[1,2,3,4,5,6,7,8,9,10]
 for i in range(10):
     adata2 = adata.copy()
     n = adata2.obs['ground_truth'].nunique()
-    adata2 = TAST_GraphST(adata = adata2, consensus = True, n_clusters = n, ground_truth = adata2.obs['ground_truth'])
+    adata2 = MCSER_GraphST(adata = adata2, n_clusters = n)
 
-    NMI = normalized_mutual_info_score(adata2.obs['consensus_mclust'].values,  adata2.obs['ground_truth'].values)
-    print('TAST NMI = %.5f' %NMI)
+    NMI = normalized_mutual_info_score(adata2.obs['MCSER_spatial_domains'].values,  adata2.obs['ground_truth'].values)
+    print('MCSER NMI = %.5f' %NMI)
     nmi_list[i] = NMI
 
 mean = np.mean(nmi_list)
-print('Average TAST NMI:', mean)
+print('Average MCSER NMI:', mean)
 
 #Write results to CSV for easy check
 results = {
         'Dataset': [section_id],
-        'TAST NMI': [mean]
+        'MCSER NMI': [mean]
     }
 results_df = pd.DataFrame(results)
 file_path = 'collected_results.csv'
