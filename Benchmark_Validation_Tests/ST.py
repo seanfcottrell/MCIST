0bf7ca109a1c7d79f86b11d168626cf4cdf4caa8
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-10-18 13:12:54 -0400

    Update ST.py

--- Benchmark_Validation_Tests/ST.py
+++ Benchmark_Validation_Tests/ST.py
@@ -5,7 +5,7 @@ warnings.filterwarnings("ignore")
 import numpy as np
 import pandas as pd
 import scanpy as sc
-from TAST_GATE import TAST_GATE
+from MCSER_GATE import MCSER_GATE
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
 #For BaristaSeq, we combine topological features with the STAGATE embedding
@@ -19,19 +19,19 @@ nmi_list=[1,2,3,4,5,6,7,8,9,10]
 for i in range(10):
     adata2 = adata.copy()
     n = adata2.obs['OHE_labels'].nunique()
-    adata2 = TAST_GATE(adata = adata2, consensus = True, n_clusters = n, spatial_rad_cutoff=2, ground_truth = adata2.obs['OHE_labels'])
+    adata2 = MCSER_GATE(adata = adata2, n_clusters = n, spatial_rad_cutoff=2)
 
-    NMI = normalized_mutual_info_score(adata2.obs['consensus_mclust'].values,  adata2.obs['OHE_labels'].values)
-    print('TAST NMI = %.5f' %NMI)
+    NMI = normalized_mutual_info_score(adata2.obs['MCSER_spatial_domains'].values,  adata2.obs['OHE_labels'].values)
+    print('MCSER NMI = %.5f' %NMI)
     nmi_list[i] = NMI
 
 mean = np.mean(nmi_list)
-print('Average TAST NMI:', mean)
+print('Average MCSER NMI:', mean)
 
 #Write results to CSV for easy check
 results = {
         'Dataset': [section_id],
-        'TAST NMI': [mean]
+        'MCSER NMI': [mean]
     }
 results_df = pd.DataFrame(results)
 file_path = 'collected_results.csv'
