2566cf24a1670a024536588df1d88f473f68e6d0
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-10-18 13:16:34 -0400

    Update Visium_DLPFC.py

--- Benchmark_Validation_Tests/Visium_DLPFC.py
+++ Benchmark_Validation_Tests/Visium_DLPFC.py
@@ -7,7 +7,7 @@ import warnings
 import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
 from sklearn.metrics.cluster import normalized_mutual_info_score
-from TAST_GATE import TAST_GATE
+from MCSER_GATE import MCSER_GATE
 
 #For DLPFC, we combine topological features with the STAGATE embedding
 #Load Data
@@ -20,27 +20,27 @@ nmi_list=[1,2,3,4,5,6,7,8,9,10]
 for i in range(10):
     adata2 = adata.copy()
     n = adata2.obs['layer'].nunique()
-    adata2 = TAST_GATE(adata = adata2, consensus = True, n_clusters = n, spatial_rad_cutoff=150, ground_truth = adata2.obs['layer'])
+    adata2 = MCSER_GATE(adata = adata2, n_clusters = n, spatial_rad_cutoff=150)
 
     obs_df = adata2.obs.dropna()
-    NMI = normalized_mutual_info_score(obs_df['consensus_mclust'],  obs_df['layer'])
-    print('TAST NMI = %.5f' %NMI)
+    NMI = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['layer'])
+    print('MCSER NMI = %.5f' %NMI)
     nmi_list[i] = NMI
 
     #Plotting
     if section_id == '151673' and i == 1:
         plt.rcParams["figure.figsize"] = (8, 8)
-        sc.pl.spatial(adata2, color='consensus_mclust', cmap = 'tab20', save='tast_'+section_id+'.png')
+        sc.pl.spatial(adata2, color='MCSER_spatial_domains', cmap = 'tab20', save='tast_'+section_id+'.png')
 
 
 #Results
 mean = np.mean(nmi_list)
-print('Average TAST NMI:', mean)
+print('Average MCSER NMI:', mean)
 
 #Write results to CSV for easy check
 results = {
         'Dataset': [section_id],
-        'TAST NMI': [mean]
+        'MCSER NMI': [mean]
     }
 results_df = pd.DataFrame(results)
 file_path = 'collected_results.csv'
