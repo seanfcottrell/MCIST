d005c2a4a492e20bb7fcf7cee27d22ab3beaa71f
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-10-18 13:11:52 -0400

    Update MERFISH.py

--- Benchmark_Validation_Tests/MERFISH.py
+++ Benchmark_Validation_Tests/MERFISH.py
@@ -6,7 +6,7 @@ import os
 import sys
 import warnings
 warnings.filterwarnings("ignore")
-from TAST_GATE import TAST_GATE
+from MCSER_GATE import MCSER_GATE
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
 #For MERFISH, we combine topological features with the STAGATE embedding
@@ -20,19 +20,19 @@ nmi_list=[1,2,3,4,5,6,7,8,9,10]
 for i in range(10):
     adata2 = adata.copy()
     n = adata2.obs['ground_truth'].nunique()
-    adata2 = TAST_GATE(adata = adata2, consensus = True, n_clusters = n, spatial_rad_cutoff=75, ground_truth = adata2.obs['ground_truth'])
+    adata2 = MCSER_GATE(adata = adata2, n_clusters = n, spatial_rad_cutoff=75)
 
-    NMI = normalized_mutual_info_score(adata2.obs['consensus_mclust'].values,  adata2.obs['ground_truth'].values)
+    NMI = normalized_mutual_info_score(adata2.obs['MCSER_spatial_domains'].values,  adata2.obs['ground_truth'].values)
     print('TAST NMI = %.5f' %NMI)
     nmi_list[i] = NMI
 
 mean = np.mean(nmi_list)
-print('Average TAST NMI:', mean)
+print('Average MCSER NMI:', mean)
 
 #Write results to CSV for easy check
 results = {
         'Dataset': [section_id],
-        'TAST NMI': [mean]
+        'MCSER NMI': [mean]
     }
 results_df = pd.DataFrame(results)
 file_path = 'collected_results.csv'
