c4248e8394f839439c7479f4ce698960f89931a1
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-12-03 20:30:25 -0500

    Update MERFISH.py

--- Benchmark_Validation_Tests/MERFISH.py
+++ Benchmark_Validation_Tests/MERFISH.py
@@ -6,35 +6,89 @@ import os
 import sys
 import warnings
 warnings.filterwarnings("ignore")
-from MCSER_GATE import MCSER_GATE
+#from MCSER_GATE import MCSER_GATE
+#from MCSER_GraphST import MCSER_GraphST
+from MCSER_SpaceFlow import MCSER_SpaceFlow
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
-#For MERFISH, we combine topological features with the STAGATE embedding
 #Load Data
 section_id = sys.argv[1]
-adata = sc.read('./Data/MERFISH/'+section_id+'.h5ad')
+adata = sc.read('/mnt/home/cottre61/GFP-GAT/STAGATE_pyG/SpatialTranscriptomics/'+section_id+'.h5ad')
 
-#Validation
-nmi_list=[1,2,3,4,5,6,7,8,9,10]
+# Validation
+n = adata.obs['ground_truth'].nunique()
+####################################################################################################
+###### GraphST ######
+# Mclust
+#adata2 = adata.copy()
+#adata2 = MCSER_GraphST(adata = adata2, n_clusters = n, clustering_algo='Mclust')
 
-for i in range(10):
-    adata2 = adata.copy()
-    n = adata2.obs['ground_truth'].nunique()
-    adata2 = MCSER_GATE(adata = adata2, n_clusters = n, spatial_rad_cutoff=75)
+#obs_df = adata2.obs.dropna()
+#NMI_Mclust_GraphST = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#print('MCIST Mclust GraphST NMI = %.5f' %NMI_Mclust_GraphST)
 
-    NMI = normalized_mutual_info_score(adata2.obs['MCSER_spatial_domains'].values,  adata2.obs['ground_truth'].values)
-    print('TAST NMI = %.5f' %NMI)
-    nmi_list[i] = NMI
+# Leiden
+#adata3 = adata.copy()
+#adata3 = MCSER_GraphST(adata = adata3, n_clusters = n, clustering_algo='Leiden')
 
-mean = np.mean(nmi_list)
-print('Average MCSER NMI:', mean)
+#obs_df = adata3.obs.dropna()
+#NMI_Leiden_GraphST = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#print('MCIST Leiden GraphST NMI = %.5f' %NMI_Leiden_GraphST)
+
+####################################################################################################
+###### GATE ######
+# Mclust
+#adata4 = adata.copy()
+#adata4 = MCSER_GATE(adata = adata4, n_clusters = n, spatial_rad_cutoff = 75, clustering_algo='Mclust')
+
+#obs_df = adata4.obs.dropna()
+#NMI_Mclust_GATE = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#print('MCIST Mclust GATE NMI = %.5f' %NMI_Mclust_GATE)
+
+# Leiden
+#adata5 = adata.copy()
+#adata5 = MCSER_GATE(adata = adata3, n_clusters = n, spatial_rad_cutoff = 75, clustering_algo='Leiden')
+
+#obs_df = adata5.obs.dropna()
+#NMI_Leiden_GATE = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#print('MCIST Leiden GATE NMI = %.5f' %NMI_Leiden_GATE)
+
+####################################################################################################
+###### SpaceFlow ######
+# Mclust 
+adata6 = adata.copy()
+adata6 = MCSER_SpaceFlow(adata = adata6, n_clusters = n, clustering_algo='Mclust')
+
+obs_df = adata6.obs.dropna()
+NMI_Mclust_SpaceFlow = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+print('MCIST Mclust SpaceFlow NMI = %.5f' %NMI_Mclust_SpaceFlow)
+# Leiden
+adata7 = adata.copy()
+adata7 = MCSER_SpaceFlow(adata = adata7, n_clusters = n, clustering_algo='Leiden')
+
+obs_df = adata7.obs.dropna()
+NMI_Leiden_SpaceFlow = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+print('MCIST Leiden SpaceFlow NMI = %.5f' %NMI_Leiden_SpaceFlow)
+####################################################################################################
+
+#if section_id == '19':
+ #       plt.rcParams["figure.figsize"] = (8, 8)
+  #      sc.pl.spatial(adata2, color='MCSER_spatial_domains', cmap = 'tab10', save='merfish'+section_id+'.png', spot_size=20)
 
 #Write results to CSV for easy check
 results = {
         'Dataset': [section_id],
-        'MCSER NMI': [mean]
-    }
+        'MCIST SpaceFlow Mclust NMI': [NMI_Mclust_SpaceFlow],
+        'MCIST SpaceFlow Leiden NMI': [NMI_Leiden_SpaceFlow]
+        }
+        #'MCIST GraphST Mclust NMI': [NMI_Mclust_GraphST],
+        #'MCIST GraphST Leiden NMI': [NMI_Leiden_GraphST],
+        #'MCIST GATE Mclust NMI': [NMI_Mclust_GATE],
+        #'MCIST GATE Leiden NMI': [NMI_Leiden_GATE]
+        #}
+
+
 results_df = pd.DataFrame(results)
-file_path = 'collected_results.csv'
+file_path = 'MERFISH_collected_results.csv'
 file_exists = os.path.exists(file_path)
 results_df.to_csv(file_path, mode='a', index=False, header=not file_exists)
