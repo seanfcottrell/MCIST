b4f04f40a1c5df12dbaf6ff86c2afa2b1cc32523
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-12-03 20:34:48 -0500

    Update MERFISH.py

--- Benchmark_Validation_Tests/MERFISH.py
+++ Benchmark_Validation_Tests/MERFISH.py
@@ -6,9 +6,9 @@ import os
 import sys
 import warnings
 warnings.filterwarnings("ignore")
-#from MCSER_GATE import MCSER_GATE
-#from MCSER_GraphST import MCSER_GraphST
-from MCSER_SpaceFlow import MCSER_SpaceFlow
+#from MCIST_GATE import MCIST_GATE
+#from MCIST_GraphST import MCIST_GraphST
+from MCIST_SpaceFlow import MCIST_SpaceFlow
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
 #Load Data
@@ -21,59 +21,59 @@ n = adata.obs['ground_truth'].nunique()
 ###### GraphST ######
 # Mclust
 #adata2 = adata.copy()
-#adata2 = MCSER_GraphST(adata = adata2, n_clusters = n, clustering_algo='Mclust')
+#adata2 = MCIST_GraphST(adata = adata2, n_clusters = n, clustering_algo='Mclust')
 
 #obs_df = adata2.obs.dropna()
-#NMI_Mclust_GraphST = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#NMI_Mclust_GraphST = normalized_mutual_info_score(obs_df['MCIST_spatial_domains'],  obs_df['ground_truth'])
 #print('MCIST Mclust GraphST NMI = %.5f' %NMI_Mclust_GraphST)
 
 # Leiden
 #adata3 = adata.copy()
-#adata3 = MCSER_GraphST(adata = adata3, n_clusters = n, clustering_algo='Leiden')
+#adata3 = MCIST_GraphST(adata = adata3, n_clusters = n, clustering_algo='Leiden')
 
 #obs_df = adata3.obs.dropna()
-#NMI_Leiden_GraphST = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#NMI_Leiden_GraphST = normalized_mutual_info_score(obs_df['MCIST_spatial_domains'],  obs_df['ground_truth'])
 #print('MCIST Leiden GraphST NMI = %.5f' %NMI_Leiden_GraphST)
 
 ####################################################################################################
 ###### GATE ######
 # Mclust
 #adata4 = adata.copy()
-#adata4 = MCSER_GATE(adata = adata4, n_clusters = n, spatial_rad_cutoff = 75, clustering_algo='Mclust')
+#adata4 = MCIST_GATE(adata = adata4, n_clusters = n, spatial_rad_cutoff = 75, clustering_algo='Mclust')
 
 #obs_df = adata4.obs.dropna()
-#NMI_Mclust_GATE = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#NMI_Mclust_GATE = normalized_mutual_info_score(obs_df['MCIST_spatial_domains'],  obs_df['ground_truth'])
 #print('MCIST Mclust GATE NMI = %.5f' %NMI_Mclust_GATE)
 
 # Leiden
 #adata5 = adata.copy()
-#adata5 = MCSER_GATE(adata = adata3, n_clusters = n, spatial_rad_cutoff = 75, clustering_algo='Leiden')
+#adata5 = MCIST_GATE(adata = adata3, n_clusters = n, spatial_rad_cutoff = 75, clustering_algo='Leiden')
 
 #obs_df = adata5.obs.dropna()
-#NMI_Leiden_GATE = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#NMI_Leiden_GATE = normalized_mutual_info_score(obs_df['MCIST_spatial_domains'],  obs_df['ground_truth'])
 #print('MCIST Leiden GATE NMI = %.5f' %NMI_Leiden_GATE)
 
 ####################################################################################################
 ###### SpaceFlow ######
 # Mclust 
 adata6 = adata.copy()
-adata6 = MCSER_SpaceFlow(adata = adata6, n_clusters = n, clustering_algo='Mclust')
+adata6 = MCIST_SpaceFlow(adata = adata6, n_clusters = n, clustering_algo='Mclust')
 
 obs_df = adata6.obs.dropna()
-NMI_Mclust_SpaceFlow = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+NMI_Mclust_SpaceFlow = normalized_mutual_info_score(obs_df['MCIST_spatial_domains'],  obs_df['ground_truth'])
 print('MCIST Mclust SpaceFlow NMI = %.5f' %NMI_Mclust_SpaceFlow)
 # Leiden
 adata7 = adata.copy()
-adata7 = MCSER_SpaceFlow(adata = adata7, n_clusters = n, clustering_algo='Leiden')
+adata7 = MCIST_SpaceFlow(adata = adata7, n_clusters = n, clustering_algo='Leiden')
 
 obs_df = adata7.obs.dropna()
-NMI_Leiden_SpaceFlow = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+NMI_Leiden_SpaceFlow = normalized_mutual_info_score(obs_df['MCIST_spatial_domains'],  obs_df['ground_truth'])
 print('MCIST Leiden SpaceFlow NMI = %.5f' %NMI_Leiden_SpaceFlow)
 ####################################################################################################
 
 #if section_id == '19':
  #       plt.rcParams["figure.figsize"] = (8, 8)
-  #      sc.pl.spatial(adata2, color='MCSER_spatial_domains', cmap = 'tab10', save='merfish'+section_id+'.png', spot_size=20)
+  #      sc.pl.spatial(adata2, color='MCIST_spatial_domains', cmap = 'tab10', save='merfish'+section_id+'.png', spot_size=20)
 
 #Write results to CSV for easy check
 results = {
