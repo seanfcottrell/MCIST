7ca6a801ce84c01abe6ba36887abcf9d495e0e67
seanfcottrell <120742124+seanfcottrell@users.noreply.github.com>
2024-12-03 20:32:12 -0500

    Update StereoSeq.py

--- Benchmark_Validation_Tests/StereoSeq.py
+++ Benchmark_Validation_Tests/StereoSeq.py
@@ -7,35 +7,83 @@ import warnings
 warnings.filterwarnings("ignore")
 from sklearn.metrics.cluster import normalized_mutual_info_score
 import multiprocessing as mp
-from MCSER_GraphST import MCSER_GraphST
+#from MCSER_GraphST import MCSER_GraphST
+#from MCSER_GATE import MCSER_GATE
+from MCSER_SpaceFlow import MCSER_SpaceFlow
 
-#For StereoSeq, we combine topological features with the GraphST embedding
 section_id = sys.argv[1]
+adata = sc.read('/mnt/home/cottre61/GFP-GAT/STAGATE_pyG/SpatialTranscriptomics/'+section_id+'.h5ad')
 
-adata = sc.read('./Data/Stereo-Seq/'+section_id+'.h5ad')
+# Validation
+n = adata.obs['ground_truth'].nunique()
+####################################################################################################
+###### GraphST ######
+# Mclust
+#adata2 = adata.copy()
+#adata2 = MCSER_GraphST(adata = adata2, n_clusters = n, clustering_algo='Mclust')
 
-#Validation
-nmi_list=[1,2,3,4,5,6,7,8,9,10]
+#obs_df = adata2.obs.dropna()
+#NMI_Mclust_GraphST = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#print('MCIST Mclust GraphST NMI = %.5f' %NMI_Mclust_GraphST)
 
-for i in range(10):
-    adata2 = adata.copy()
-    n = adata2.obs['ground_truth'].nunique()
-    adata2 = MCSER_GraphST(adata = adata2, n_clusters = n)
+# Leiden
+#adata3 = adata.copy()
+#adata3 = MCSER_GraphST(adata = adata3, n_clusters = n, clustering_algo='Leiden')
 
-    NMI = normalized_mutual_info_score(adata2.obs['MCSER_spatial_domains'].values,  adata2.obs['ground_truth'].values)
-    print('MCSER NMI = %.5f' %NMI)
-    nmi_list[i] = NMI
+#obs_df = adata3.obs.dropna()
+#NMI_Leiden_GraphST = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#print('MCIST Leiden GraphST NMI = %.5f' %NMI_Leiden_GraphST)
 
-mean = np.mean(nmi_list)
-print('Average MCSER NMI:', mean)
+####################################################################################################
+###### GATE ######
+# Mclust
+#adata4 = adata.copy()
+#adata4 = MCSER_GATE(adata = adata4, n_clusters = n, spatial_rad_cutoff = 2, clustering_algo='Mclust')
+
+#obs_df = adata4.obs.dropna()
+#NMI_Mclust_GATE = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#print('MCIST Mclust GATE NMI = %.5f' %NMI_Mclust_GATE)
+
+# Leiden
+#adata5 = adata.copy()
+#adata5 = MCSER_GATE(adata = adata3, n_clusters = n, spatial_rad_cutoff = 2, clustering_algo='Leiden')
+
+#obs_df = adata5.obs.dropna()
+#NMI_Leiden_GATE = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+#print('MCIST Leiden GATE NMI = %.5f' %NMI_Leiden_GATE)
+
+####################################################################################################
+###### SpaceFlow ######
+# Mclust 
+adata6 = adata.copy()
+adata6 = MCSER_SpaceFlow(adata = adata6, n_clusters = n, clustering_algo='Mclust')
+
+obs_df = adata6.obs.dropna()
+NMI_Mclust_SpaceFlow = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+print('MCIST Mclust SpaceFlow NMI = %.5f' %NMI_Mclust_SpaceFlow)
+# Leiden
+adata7 = adata.copy()
+adata7 = MCSER_SpaceFlow(adata = adata7, n_clusters = n, clustering_algo='Leiden')
+
+obs_df = adata7.obs.dropna()
+NMI_Leiden_SpaceFlow = normalized_mutual_info_score(obs_df['MCSER_spatial_domains'],  obs_df['ground_truth'])
+print('MCIST Leiden SpaceFlow NMI = %.5f' %NMI_Leiden_SpaceFlow)
+####################################################################################################
 
 #Write results to CSV for easy check
 results = {
         'Dataset': [section_id],
-        'MCSER NMI': [mean]
+        'MCIST SpaceFlow Mclust NMI': [NMI_Mclust_SpaceFlow],
+        'MCIST SpaceFlow Leiden NMI': [NMI_Leiden_SpaceFlow]
     }
+        #'MCIST GraphST Mclust NMI': [NMI_Mclust_GraphST],
+        #'MCIST GraphST Leiden NMI': [NMI_Leiden_GraphST],
+        #'MCIST GATE Mclust NMI': [NMI_Mclust_GATE],
+        #'MCIST GATE Leiden NMI': [NMI_Leiden_GATE]
+        #}
+        
+
 results_df = pd.DataFrame(results)
-file_path = 'collected_results.csv'
+file_path = 'StereoSeq_sf.csv'
 file_exists = os.path.exists(file_path)
 results_df.to_csv(file_path, mode='a', index=False, header=not file_exists)
-
